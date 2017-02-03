6a694e49a17cbd9ef9f493e1cdc2f1352397f127
李昌文 <lcw2004@163.com>
2017-02-03 17:16:55 +0800

    添加插件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+Vue.use(Vuex)
+
 import App from './App'
 
 /* eslint-disable no-new */
