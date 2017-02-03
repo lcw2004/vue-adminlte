1b0a1b38b29d6fc5c3b505983cff567e423f8705
李昌文 <lcw2004@163.com>
2017-02-03 22:41:22 +0800

    拆分组件

---
+++ src/components/layout/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <MainHead></MainHead>
+      <MainSlidebar></MainSlidebar>
+      <MainContent></MainContent>
+      <MainFooter><MainFooter>
+
+      <!-- Control Sidebar -->
+      <ControlSidebar></ControlSidebar>
+      <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+      <div class="control-sidebar-bg"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MainHead from './MainHead'
+import MainSlidebar from './MainSlidebar'
+import MainFooter from './MainFooter'
+import MainContent from './MainContent'
+import ControlSidebar from './ControlSidebar'
+
+export default {
+  name: 'home',
+  components: {
+    MainHead,
+    MainSlidebar,
+    MainFooter,
+    MainContent,
+    ControlSidebar
+  }
+}
+</script>
