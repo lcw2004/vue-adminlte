3d205ce4d6c299eba071e08073fad40bce47015f
李昌文 <lcw2004@163.com>
2017-02-07 00:34:56 +0800

    1

--- src/components/bidding/AddBidding.vue
+++ src/components/bidding/AddBidding.vue
@@ -4,11 +4,11 @@
       <div class="col-md-12">
         <div class="box box-primary">
           <div class="box-header with-border">
-            <div class="row">
+            <div class="col-md-12">
               <div class="puastep_tab">
-                <a>1.基本信息</a>
+                <a class="puastep_tab_now">1.基本信息</a>
                 <span>&gt;</span>
-                <a class="puastep_tab_now">2.标的信息</a>
+                <a>2.标的信息</a>
                 <span>&gt;</span>
                 <a>3.投标单位</a>
                 <span>&gt;</span>
@@ -23,25 +23,8 @@
             </div>
           </div>
           <form class="form-horizontal">
-            <div class="box-body">
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10">
-                  <input type="email" id="inputEmail3" placeholder="Email" class="form-control"></div>
-              </div>
-              <div class="form-group">
-                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                <div class="col-sm-10">
-                  <input type="password" id="inputPassword3" placeholder="Password" class="form-control"></div>
-              </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <div class="checkbox">
-                    <label>
-                      <input type="checkbox">Remember me</label></div>
-                </div>
-              </div>
-            </div>
+            <BaseInfo/>
+
             <div class="box-footer">
               <div class="col-md-4 col-sm-0"></div>
               <div class="col-md-2 col-sm-2"><button type="submit" class="btn btn-block btn-default">上一步</button></div>
@@ -55,8 +38,13 @@
 </template>
 
 <script>
+import BaseInfo from './edit/BaseInfo'
+
 export default {
-  name: 'AddBidding'
+  name: 'AddBidding',
+  components: {
+    BaseInfo
+  }
 }
 </script>
 
