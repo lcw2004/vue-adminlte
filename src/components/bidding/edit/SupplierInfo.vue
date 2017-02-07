86292ba5b64aa596fe0a4a5795b75749f3b4332b
李昌文 <lcw2004@163.com>
2017-02-08 00:35:25 +0800

    ·1

--- src/components/bidding/edit/SupplierInfo.vue
+++ src/components/bidding/edit/SupplierInfo.vue
@@ -1,11 +1,64 @@
 <template>
-  <div>
-    SupplierInfo
-  </div>
+<div class="box-body">
+  <button type="button" class="btn btn-info btn-sm pull-right" @click="config.show = true">
+    <i class="fa fa-plus"></i>  添加投标单位
+  </button>
+  <table class="table table-bordered">
+    <tbody>
+      <tr>
+        <th style="width: 10px">#</th>
+        <th>公司名称</th>
+        <th>联系人</th>
+        <th>手机号码</th>
+        <th>操作</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Update software</td>
+        <td>张三</td>
+        <td>12345678901</td>
+        <td>
+          <a>删除</a>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Clean database</td>
+        <td>李四</td>
+        <td>12345678901</td>
+        <td>
+          <a>删除</a>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Cron job running</td>
+        <td>王五</td>
+        <td>12345678901</td>
+        <td>
+          <a>删除</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <SelectSupplier :config="config">
+</div>
 </template>
 
 <script>
+import SelectSupplier from '../../modal/SelectSupplier'
 export default {
-  name: 'SupplierInfo'
+  name: 'SupplierInfo',
+  components: {
+    SelectSupplier
+  },
+  data: function () {
+    return {
+      config: {
+        show: false,
+        title: '选择投标单位'
+      }
+    }
+  }
 }
 </script>
---
+++ src/components/modal/ModalBase.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="modal" v-show="config.show" style="display: block">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="config.show = false">
+          <span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" v-text="config.title"></h4>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default " data-dismiss="modal" @click="config.show = false">取消</button>
+        <button type="button" class="btn btn-primary" @click="select()">确认</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SelectSupplier',
+  props: {
+    // 模态窗属性配置
+    config: {
+      type: Object,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+    select: function () {
+
+    }
+  }
+}
+</script>
