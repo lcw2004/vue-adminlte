a9ed6b6fa72c3acb1291112d321401241365ded7
李昌文 <lcw2004@163.com>
2017-02-21 23:02:29 +0800

    提取fixTable

--- src/views/bidding/scence/BiddingCompare.vue
+++ src/views/bidding/scence/BiddingCompare.vue
@@ -75,6 +75,7 @@
 
 <script>
 import $ from 'jquery'
+import fixTable from '../../../utils/fixTable'
 
 export default {
   name: 'BiddingCompare',
@@ -126,7 +127,6 @@ export default {
         return 0
       })
 
-      let newTotalMoney = []
       let index = 1
       for (let money of totalMoney) {
         if (money) {
@@ -138,99 +138,4 @@ export default {
   }
 }
 
-function fixTable (tableId, fixColumnNumber, width, height) {
-  // 查找_tableLayout，如果没有，则添加
-  var tableLayout = $('#' + tableId + '_tableLayout')
-  if (tableLayout.length !== 0) {
-    tableLayout.before($('#' + tableId))
-    tableLayout.empty()
-  } else {
-    $('#' + tableId).after('<div id="' + tableId + '_tableLayout" style="overflow:hidden;height:' + height + 'px; width:' + width + 'px;"></div>')
-  }
-  $('<div id="' + tableId + '_tableFix"></div>' + '<div id="' + tableId + '_tableHead"></div>' + '<div id="' + tableId + '_tableColumn"></div>' + '<div id="' + tableId + '_tableData"></div>').appendTo('#' + tableId + '_tableLayout')
-
-  // --------------
-  // 旧table
-  var oldtable = $('#' + tableId)
-
-  var tableFixClone = oldtable.clone(true)
-  tableFixClone.attr('id', tableId + '_tableFixClone')
-  var tableFix = $('#' + tableId + '_tableFix')
-  tableFix.append(tableFixClone)
-
-  var tableHeadClone = oldtable.clone(true)
-  tableHeadClone.attr('id', tableId + '_tableHeadClone')
-  var tableHead = $('#' + tableId + '_tableHead')
-  tableHead.append(tableHeadClone)
-
-  var tableColumnClone = oldtable.clone(true)
-  tableColumnClone.attr('id', tableId + '_tableColumnClone')
-  var tableColumn = $('#' + tableId + '_tableColumn')
-  tableColumn.append(tableColumnClone)
-  var tableData = $('#' + tableId + '_tableData')
-  tableData.append(oldtable)
-  $('#' + tableId + '_tableLayout table').each(function () {
-    $(this).css('margin', '0')
-  })
-  // --------------
-
-  // 计算表格固定部分的宽度和高度
-  var headHeight = $('#' + tableId + '_tableHead thead').height() + 2
-  tableHead.css('height', headHeight)
-  tableFix.css('height', headHeight)
-  var columnsWidth = 0
-  $('#' + tableId + '_tableColumn tr:last td:lt(' + fixColumnNumber + ')').each(function () {
-    columnsWidth += $(this).outerWidth(true)
-  })
-
-  tableColumn.css('width', columnsWidth)
-  tableFix.css('width', columnsWidth)
-  tableData.scroll(function () {
-    tableHead.scrollLeft(tableData.scrollLeft())
-    tableColumn.scrollTop(tableData.scrollTop())
-  })
-
-  tableFix.css({
-    'overflow': 'hidden',
-    'position': 'relative',
-    'z-index': '50',
-    'background-color': '#f9f9f9'
-  })
-  tableHead.css({
-    'overflow': 'hidden',
-    'width': width - 17,
-    'position': 'relative',
-    'z-index': '45',
-    'background-color': '#ffffff'
-  })
-  tableColumn.css({
-    'overflow': 'hidden',
-    'height': height - 17,
-    'position': 'relative',
-    'z-index': '40',
-    'background-color': '#f9f9f9'
-  })
-  tableData.css({
-    'overflow-x': 'scroll',
-    'overflow-y': 'hidden',
-    'width': width,
-    'height': height,
-    'position': 'relative',
-    'z-index': '35'
-  })
-
-  if (tableHead.width() > $('#' + tableId + '_tableFix table').width()) {
-    tableHead.css('width', $('#' + tableId + '_tableFix table').width())
-    tableData.css('width', $('#' + tableId + '_tableFix table').width() + 17)
-  }
-  if (tableColumn.height() > $('#' + tableId + '_tableColumn table').height()) {
-    tableColumn.css('height', $('#' + tableId + '_tableColumn table').height())
-    tableData.css('height', $('#' + tableId + '_tableColumn table').height() + 17)
-  }
-
-  tableFix.offset($('#' + tableId + '_tableLayout').offset())
-  tableHead.offset($('#' + tableId + '_tableLayout').offset())
-  tableColumn.offset($('#' + tableId + '_tableLayout').offset())
-  tableData.offset($('#' + tableId + '_tableLayout').offset())
-}
 </script>
