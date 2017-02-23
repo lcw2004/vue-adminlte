55e8966fd9def9c36e6ab67352171a34a28f5f8f
李昌文 <lcw2004@163.com>
2017-02-23 23:42:03 +0800

    开标一览表

--- src/views/bidding/scence/BiddingCompare.vue
+++ src/views/bidding/scence/BiddingCompare.vue
@@ -3,29 +3,82 @@
   <div class="row">
     <div class="col-md-12">
       <div class="box">
+        <div class="box-header with-border">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="box-title">
+                开标一览表
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="control-group pull-right">
+                <label class="control-label">标的详情</label>
+                <div class="radio inline">
+                  <label>
+                    <input type="radio" value="1" v-model="showSubjectInfo">
+                    是
+                  </label>
+                </div>
+                <div class="radio inline">
+                  <label>
+                    <input type="radio" value="0" v-model="showSubjectInfo">
+                    否
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="control-group pull-right">
+                <label class="control-label">报价</label>
+                <div class="checkbox inline">
+                  <label>
+                    <input type="checkbox" value="1" v-model="showPrices">
+                    含税价
+                  </label>
+                </div>
+                <div class="checkbox inline">
+                  <label>
+                    <input type="checkbox" value="2" v-model="showPrices">
+                    剔税价
+                  </label>
+                </div>
+                <div class="checkbox inline">
+                  <label>
+                    <input type="checkbox" value="3" v-model="showPrices">
+                    税率
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div class="box-body">
           <div class="row">
             <div class="col-md-12" style="overflow:auto;" id="flagDiv">
-              <table id="myTable" class="table table-bordered" style="width: 1500px">
+              <table id="myTable" class="table table-bordered">
                 <thead>
                   <tr>
                     <td rowspan="2">序号</td>
                     <td rowspan="2">标的名称</td>
+
+                    <template v-if="showSubjectInfo == '1'">
                     <td rowspan="2">标的编码</td>
                     <td rowspan="2">标的数量</td>
                     <td rowspan="2">计量单位</td>
                     <td rowspan="2">规格型号</td>
+                    </template>
 
                     <template v-for="supplier of supplierPricesSorted.suppliers">
-                    <td colspan="4">{{ supplier.name }}</td>
+                    <td :colspan="showPrices.length + 1">{{ supplier.name }}</td>
                     </template>
                   </tr>
                   <tr>
                     <template v-for="supplier of supplierPricesSorted.suppliers">
                       <td>最终报价</td>
-                      <td>含税价</td>
-                      <td>剔税价</td>
-                      <td>税率</td>
+                      <td v-if="showPriceType('1')">含税价</td>
+                      <td v-if="showPriceType('2')">剔税价</td>
+                      <td v-if="showPriceType('3')">税率</td>
                     </template>
                   </tr>
                 </thead>
@@ -34,37 +87,43 @@
                   <tr v-for="(supplierPrice, index) of supplierPricesSorted.prices">
                     <td>{{ index + 1 }}</td>
                     <td>{{ supplierPrice.subjectInfo.name }}</td>
+
+                    <template v-if="showSubjectInfo == '1'">
                     <td>{{ supplierPrice.subjectInfo.code }}</td>
                     <td>{{ supplierPrice.subjectInfo.number }}</td>
                     <td>{{ supplierPrice.subjectInfo.unit }}</td>
                     <td></td>
+                    </template>
 
                     <template v-for="(value, key, index) of supplierPrice.supplierPrices">
                     <td>
                       {{ value.containTax }}
                       <span class="label label-success" v-if="value.sortNum == 1">低</span>
                     </td>
-                    <td>{{ value.containTax }}</td>
-                    <td>{{ value.notContainTax }}</td>
-                    <td>{{ value.tax }}</td>
+                    <td v-if="showPriceType('1')">{{ value.containTax }}</td>
+                    <td v-if="showPriceType('2')">{{ value.notContainTax }}</td>
+                    <td v-if="showPriceType('3')">{{ value.tax }}</td>
                     </template>
                   </tr>
 
                   <tr>
                     <td></td>
                     <td>总金额</td>
+
+                    <template v-if="showSubjectInfo == '1'">
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
+                    </template>
 
                     <template v-for="supplier of supplierPricesSorted.suppliers">
                       <td>{{ totalMoneySorted.get(supplier.id).total }}
                         <SupplierPriceUnit :value="totalMoneySorted.get(supplier.id).sortNum"></SupplierPriceUnit>
                       </td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
+                      <td v-if="showPriceType('1')"></td>
+                      <td v-if="showPriceType('2')"></td>
+                      <td v-if="showPriceType('3')"></td>
                     </template>
                   </tr>
                 </tbody>
@@ -90,12 +149,36 @@ export default {
   },
   data: function () {
     return {
+      showSubjectInfo: 0,
+      showPrices: []
     }
   },
   mounted: function () {
-    var width = $('#flagDiv').width()
-    var heigth = $('#flagDiv').height() + 20
-    fixTable('myTable', 2, width, heigth)
+    this.initTable()
+  },
+  watch: {
+    'showSubjectInfo': {
+      handler: function () {
+        this.initTable()
+      }
+    },
+    'showPrices': {
+      handler: function () {
+        this.initTable()
+      }
+    }
+  },
+  methods: {
+    initTable: function () {
+      fixTable
+      // 样式切换的时候错乱，暂时屏蔽
+      // let width = $('#flagDiv').width()
+      // let heigth = $('#flagDiv').height() + 20
+      // fixTable('myTable', 2, width, heigth)
+    },
+    showPriceType: function (index) {
+      return $.inArray(index, this.showPrices) !== -1
+    }
   },
   computed: {
     supplierPrices: function () {
@@ -171,3 +254,10 @@ export default {
 }
 
 </script>
+
+<style type="text/css">
+td {
+  word-break: keep-all;
+  white-space:nowrap;
+}
+</style>
