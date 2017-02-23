e4d589ef4fd41880758732f57fdc631b58c1736b
李昌文 <lcw2004@163.com>
2017-02-24 00:06:04 +0800

    路由分模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,14 @@
 import Desktop from '../views/layout/Desktop'
-import AddBidding from '../views/bidding/AddBidding'
-import BiddingAudit from '../views/bidding/BiddingAudit'
-import BiddingManage from '../views/bidding/BiddingManage'
-import BiddingPermission from '../views/bidding/BiddingPermission'
-import MyBidding from '../views/bidding/MyBidding'
 
-import BiddingPermissionInfo from '../views/bidding/BiddingPermissionInfo'
-import AuditProgress from '../views/bidding/AuditProgress'
-import BiddingClarify from '../views/bidding/BiddingClarify'
-import BiddingInfo from '../views/bidding/BiddingInfo'
-import BiddingPreAward from '../views/bidding/BiddingPreAward'
-import BiddingScene from '../views/bidding/BiddingScene'
-import BiddingCompare from '../views/bidding/scence/BiddingCompare'
-
-import ClarifyInfo from '../views/bidding/clarify/ClarifyInfo'
-
-import PurchaseType from '../views/system/purchase/PurchaseType'
-import ProfessionType from '../views/system/profession/ProfessionType'
-import NoticeManage from '../views/system/notice/NoticeManage'
-import NoticeTreeForm from '../views/system/notice/NoticeTreeForm'
+import bidding from './bidding'
+import system from './system'
 
 const router = {
   routes: [
     { path: '/', component: Desktop },
-    { path: '/add-bidding', component: AddBidding },
-    { path: '/bidding-audit', component: BiddingAudit },
-    { path: '/bidding-manage', component: BiddingManage },
-    { path: '/bidding-permission', component: BiddingPermission },
-    { path: '/my-bidding', component: MyBidding },
-    { path: '/bidding-permission-info', component: BiddingPermissionInfo },
-    { path: '/audit-progress', component: AuditProgress },
-    { path: '/bidding-clarify', component: BiddingClarify },
-    { path: '/bidding-clarify-info', component: ClarifyInfo },
-    { path: '/bidding-info', component: BiddingInfo },
-    { path: '/bidding-pre-award', component: BiddingPreAward },
-    { path: '/bidding-scene', component: BiddingScene },
-    { path: '/bidding-compare', component: BiddingCompare },
 
-    { path: '/purchase-type', component: PurchaseType },
-    { path: '/profession-type', component: ProfessionType },
-    { path: '/notice-namage', component: NoticeManage },
-    { path: '/notice-tree-form', component: NoticeTreeForm }
+    ...bidding,
+    ...system
   ]
 }
 
