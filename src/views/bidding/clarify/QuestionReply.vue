6715e0894d85fcdbc2352584bf37bdd2f212b0c2
李昌文 <lcw2004@163.com>
2017-03-06 22:55:24 +0800

    供应商提问答复

--- src/views/bidding/clarify/QuestionReply.vue
+++ src/views/bidding/clarify/QuestionReply.vue
@@ -4,25 +4,20 @@
     <div class="col-md-12">
       <div class="box">
         <div class="box-header">
-          <h3 class="box-title">澄清详情</h3>
+          <h3 class="box-title">李四：关于某某项目的疑问</h3>
         </div>
         <div class="box-body">
+          <blockquote>
+            <p>关于这个项目我在某些地方有些疑问。。。。</p>
+          </blockquote>
 
           <form class="form-horizontal">
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-12">
                 <div class="form-group">
-                  <label class="col-md-4 control-label">问题</label>
-                  <div class="col-md-8">
-                    <p class="form-control-static">关于某某项目的澄清</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label class="col-md-4 control-label">供应商</label>
-                  <div class="col-md-8">
-                    <p class="form-control-static">李四</p>
+                  <label class="col-md-1 control-label">答复</label>
+                  <div class="col-md-10">
+                    <Editor></Editor>
                   </div>
                 </div>
               </div>
@@ -31,9 +26,9 @@
             <div class="row">
               <div class="col-md-12">
                 <div class="form-group">
-                  <label class="col-md-2 control-label">描述</label>
-                  <div class="col-md-9">
-                    <Editor></Editor>
+                  <label class="col-md-1 control-label">附件</label>
+                  <div class="col-md-10">
+                    <button type="button" class="btn btn-primary"><i class="fa fa-paperclip"></i> 上传附件</button>
                   </div>
                 </div>
               </div>
@@ -43,11 +38,15 @@
 
         <div class="box-footer">
           <div class="col-md-2 col-sm-0"></div>
+          <div class="col-md-2 col-sm-2"><button class="btn btn-block btn-primary">回复</button></div>
           <div class="col-md-2 col-sm-2"><button class="btn btn-block btn-default" @click="$router.go(-1)">返回</button></div>
         </div>
       </div>
     </div>
   </div>
+
+
+
 </section>
 </template>
 
