28162c7f9971926e1af9bdb02935937020b54c49
李昌文 <lcw2004@163.com>
2017-03-11 21:22:22 +0800

    银行账户配置

--- src/router/system/index.js
+++ src/router/system/index.js
@@ -3,11 +3,13 @@ import ProfessionType from '../../views/system/profession/ProfessionType'
 import NoticeManage from '../../views/system/notice/NoticeManage'
 import NoticeTreeForm from '../../views/system/notice/NoticeTreeForm'
 import RoomManage from '../../views/system/room/RoomManage'
+import SettingManage from '../../views/system/setting/SettingManage'
 
 export default [
   { path: '/system/purchase-type', component: PurchaseType },
   { path: '/system/profession-type', component: ProfessionType },
   { path: '/system/notice', component: NoticeManage },
   { path: '/system/notice-tree-form', component: NoticeTreeForm },
-  { path: '/system/room', component: RoomManage }
+  { path: '/system/room', component: RoomManage },
+  { path: '/system/setting', component: SettingManage }
 ]
--- src/views/layout/slidebar/LeftMenu.vue
+++ src/views/layout/slidebar/LeftMenu.vue
@@ -68,6 +68,7 @@
         <li><a><i class="fa fa-circle-o"></i><router-link to='/system/profession-type'>专业类别管理</router-link></a></li>
         <li><a><i class="fa fa-circle-o"></i><router-link to='/system/notice'>须知文件管理（投标、注册）</router-link></a></li>
         <li><a><i class="fa fa-circle-o"></i><router-link to='/system/room'>评标室管理</router-link></a></li>
+        <li><a><i class="fa fa-circle-o"></i><router-link to='/system/setting'>银行账户配置</router-link></a></li>
       </ul>
     </li>
     <li class="treeview">
