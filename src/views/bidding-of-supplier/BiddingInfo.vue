386edceae49a64bcb3bf71066bee42075235f319
李昌文 <lcw2004@163.com>
2017-03-12 20:18:24 +0800

    TimeLine2

--- src/views/bidding-of-supplier/BiddingInfo.vue
+++ src/views/bidding-of-supplier/BiddingInfo.vue
@@ -3,7 +3,6 @@
   <div class="row">
     <div class="col-md-6">
       <TimeLine></TimeLine>
-
       <TimeLine1></TimeLine1>
     </div>
 
@@ -18,12 +17,14 @@
 import ProjectInfo from './ProjectInfo'
 import TimeLine from './TimeLine'
 import TimeLine1 from './TimeLine1'
+import TimeLine2 from './TimeLine2'
 
 export default {
   components: {
     ProjectInfo,
     TimeLine,
-    TimeLine1
+    TimeLine1,
+    TimeLine2
   },
   data: function () {
     return {
