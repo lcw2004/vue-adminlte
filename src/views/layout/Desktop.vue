84c0f642092bafc5d58036d6d9c144f017791ca2
李昌文 <lcw2004@163.com>
2017-03-12 11:46:14 +0800

    首页

--- src/views/layout/Desktop.vue
+++ src/views/layout/Desktop.vue
@@ -1,6 +1,11 @@
 <template>
 <section class="content">
   <div class="row">
+    <div class="col-md-12">
+      <h4 class="page-header">负责人首页</h4>
+    </div>
+  </div>
+  <div class="row">
     <div class="col-lg-3 col-xs-6">
       <div class="small-box bg-yellow">
         <div class="inner">
@@ -41,12 +46,44 @@
       <div class="small-box bg-red">
         <div class="inner">
           <h3>65</h3>
-          <p>Unique Visitors</p>
+          <p>项目统计</p>
         </div>
         <div class="icon">
           <i class="ion ion-pie-graph"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="#" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <h4 class="page-header">供应商首页</h4>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-3 col-xs-6">
+      <div class="small-box bg-yellow">
+        <div class="inner">
+          <h3>6</h3>
+          <p>我的投标项目</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-person-add"></i>
+        </div>
+        <a href="#" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <div class="col-lg-3 col-xs-6">
+      <div class="small-box bg-aqua">
+        <div class="inner">
+          <h3>3</h3>
+          <p>招标项目公告</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-bag"></i>
+        </div>
+        <a href="#" class="small-box-footer">查看 <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
   </div>
