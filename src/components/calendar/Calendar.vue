2260b3fb4cb878b980537ef1043f3cd52402b03e
李昌文 <lcw2004@163.com>
2017-03-13 22:23:42 +0800

    日历组件可用

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="calendar"></div>
+<div id="calendar">
+</div>
 </template>
 
 <script>
@@ -9,72 +10,96 @@ require('fullcalendar')
 export default {
   name: 'Calendar',
   mounted () {
+    let date = new Date()
+    let d = date.getDate()
+    let m = date.getMonth()
+    let y = date.getFullYear()
     $('#calendar').fullCalendar({
+      locale: 'zh-cn',
       header: {
         left: 'prev,next today',
         center: 'title',
-        right: 'month,basicWeek,basicDay'
+        right: 'month,agendaWeek,agendaDay'
       },
-      defaultDate: '2017-02-12',
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
+      buttonText: {
+        today: 'today',
+        month: 'month',
+        week: 'week',
+        day: 'day'
+      },
       events: [
         {
           title: 'All Day Event',
-          start: '2017-02-01'
+          start: new Date(y, m, 1),
+          backgroundColor: '#f56954', // red
+          borderColor: '#f56954' // red
         },
         {
           title: 'Long Event',
-          start: '2017-02-07',
-          end: '2017-02-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-02-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-02-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2017-02-11',
-          end: '2017-02-13'
+          start: new Date(y, m, d - 5),
+          end: new Date(y, m, d - 2),
+          backgroundColor: '#f39c12', // yellow
+          borderColor: '#f39c12' // yellow
         },
         {
           title: 'Meeting',
-          start: '2017-02-12T10:30:00',
-          end: '2017-02-12T12:30:00'
+          start: new Date(y, m, d, 10, 30),
+          allDay: false,
+          backgroundColor: '#0073b7', // Blue
+          borderColor: '#0073b7' // Blue
         },
         {
           title: 'Lunch',
-          start: '2017-02-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2017-02-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2017-02-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2017-02-12T20:00:00'
+          start: new Date(y, m, d, 12, 0),
+          end: new Date(y, m, d, 14, 0),
+          allDay: false,
+          backgroundColor: '#00c0ef', // Info (aqua)
+          borderColor: '#00c0ef' // Info (aqua)
         },
         {
           title: 'Birthday Party',
-          start: '2017-02-13T07:00:00'
+          start: new Date(y, m, d + 1, 19, 0),
+          end: new Date(y, m, d + 1, 22, 30),
+          allDay: false,
+          backgroundColor: '#00a65a', // Success (green)
+          borderColor: '#00a65a' // Success (green)
         },
         {
           title: 'Click for Google',
+          start: new Date(y, m, 28),
+          end: new Date(y, m, 29),
           url: 'http://google.com/',
-          start: '2017-02-28'
+          backgroundColor: '#3c8dbc', // Primary (light-blue)
+          borderColor: '#3c8dbc' // Primary (light-blue)
         }
-      ]
+      ],
+      editable: true,
+      droppable: true, // this allows things to be dropped onto the calendar !!!
+      drop: function (date, allDay) {
+        // this function is called when something is dropped
+
+        // retrieve the dropped element's stored Event Object
+        var originalEventObject = $(this).data('eventObject')
+
+        // we need to copy it, so that multiple events don't have a reference to the same object
+        var copiedEventObject = $.extend({}, originalEventObject)
+
+        // assign it the date that was reported
+        copiedEventObject.start = date
+        copiedEventObject.allDay = allDay
+        copiedEventObject.backgroundColor = $(this).css('background-color')
+        copiedEventObject.borderColor = $(this).css('border-color')
+
+        // render the event on the calendar
+        // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+        $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
+
+        // is the "remove after drop" checkbox checked?
+        if ($('#drop-remove').is(':checked')) {
+          // if so, remove the element from the "Draggable Events" list
+          $(this).remove()
+        }
+      }
     })
   }
 }
