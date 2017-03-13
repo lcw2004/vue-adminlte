ec9f84eeafb0c76ef1b21d7221b6103cb2059e90
李昌文 <lcw2004@163.com>
2017-03-13 22:43:42 +0800

    cailendar

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="calendar">
+<div :id="id">
 </div>
 </template>
 
@@ -9,98 +9,50 @@ require('fullcalendar')
 
 export default {
   name: 'Calendar',
+  props: {
+    events: {
+      type: Array,
+      require: false
+    }
+  },
   mounted () {
-    let date = new Date()
-    let d = date.getDate()
-    let m = date.getMonth()
-    let y = date.getFullYear()
-    $('#calendar').fullCalendar({
-      locale: 'zh-cn',
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+    this.init()
+  },
+  data: function () {
+    return {
+      id: 'Calendar'
+    }
+  },
+  watch: {
+    events: {
+      handler: function () {
+        this.init()
       },
-      buttonText: {
-        today: 'today',
-        month: 'month',
-        week: 'week',
-        day: 'day'
-      },
-      events: [
-        {
-          title: 'All Day Event',
-          start: new Date(y, m, 1),
-          backgroundColor: '#f56954', // red
-          borderColor: '#f56954' // red
+      deep: true
+    }
+  },
+  methods: {
+    init () {
+      console.log('init ')
+      let self = this
+      $('#' + this.id).fullCalendar({
+        locale: 'zh-cn',
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
         },
-        {
-          title: 'Long Event',
-          start: new Date(y, m, d - 5),
-          end: new Date(y, m, d - 2),
-          backgroundColor: '#f39c12', // yellow
-          borderColor: '#f39c12' // yellow
+        buttonText: {
+          today: 'today',
+          month: 'month',
+          week: 'week',
+          day: 'day'
         },
-        {
-          title: 'Meeting',
-          start: new Date(y, m, d, 10, 30),
-          allDay: false,
-          backgroundColor: '#0073b7', // Blue
-          borderColor: '#0073b7' // Blue
-        },
-        {
-          title: 'Lunch',
-          start: new Date(y, m, d, 12, 0),
-          end: new Date(y, m, d, 14, 0),
-          allDay: false,
-          backgroundColor: '#00c0ef', // Info (aqua)
-          borderColor: '#00c0ef' // Info (aqua)
-        },
-        {
-          title: 'Birthday Party',
-          start: new Date(y, m, d + 1, 19, 0),
-          end: new Date(y, m, d + 1, 22, 30),
-          allDay: false,
-          backgroundColor: '#00a65a', // Success (green)
-          borderColor: '#00a65a' // Success (green)
-        },
-        {
-          title: 'Click for Google',
-          start: new Date(y, m, 28),
-          end: new Date(y, m, 29),
-          url: 'http://google.com/',
-          backgroundColor: '#3c8dbc', // Primary (light-blue)
-          borderColor: '#3c8dbc' // Primary (light-blue)
-        }
-      ],
-      editable: true,
-      droppable: true, // this allows things to be dropped onto the calendar !!!
-      drop: function (date, allDay) {
-        // this function is called when something is dropped
-
-        // retrieve the dropped element's stored Event Object
-        var originalEventObject = $(this).data('eventObject')
-
-        // we need to copy it, so that multiple events don't have a reference to the same object
-        var copiedEventObject = $.extend({}, originalEventObject)
-
-        // assign it the date that was reported
-        copiedEventObject.start = date
-        copiedEventObject.allDay = allDay
-        copiedEventObject.backgroundColor = $(this).css('background-color')
-        copiedEventObject.borderColor = $(this).css('border-color')
-
-        // render the event on the calendar
-        // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-        $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
-
-        // is the "remove after drop" checkbox checked?
-        if ($('#drop-remove').is(':checked')) {
-          // if so, remove the element from the "Draggable Events" list
-          $(this).remove()
-        }
-      }
-    })
+        events: self.events,
+        editable: false,
+        droppable: false
+      })
+    }
   }
 }
 </script>
--- src/views/bidding/BiddingCalendar.vue
+++ src/views/bidding/BiddingCalendar.vue
@@ -7,12 +7,11 @@
     <div class="box-body">
       <div class="row">
         <div class="col-md-12">
-          <Calendar></Calendar>
+          <Calendar :events="events"></Calendar>
         </div>
       </div>
     </div>
     <div class="box-footer">
-
     </div>
   </div>
 </section>
@@ -20,6 +19,71 @@
 
 <script>
 export default {
-  props: {}
+  props: {},
+  data: function () {
+    return {
+      events: []
+    }
+  },
+  mounted () {
+    this.loadEvents()
+  },
+  methods: {
+    loadEvents () {
+      let date = new Date()
+      let d = date.getDate()
+      let m = date.getMonth()
+      let y = date.getFullYear()
+
+      let events = [
+        {
+          title: 'All Day Event 1',
+          start: new Date(y, m, 1),
+          backgroundColor: '#f56954', // red
+          borderColor: '#f56954' // red
+        },
+        {
+          title: 'Long Event',
+          start: new Date(y, m, d - 5),
+          end: new Date(y, m, d - 2),
+          backgroundColor: '#f39c12', // yellow
+          borderColor: '#f39c12' // yellow
+        },
+        {
+          title: 'Meeting',
+          start: new Date(y, m, d, 10, 30),
+          allDay: false,
+          backgroundColor: '#0073b7', // Blue
+          borderColor: '#0073b7' // Blue
+        },
+        {
+          title: 'Lunch',
+          start: new Date(y, m, d, 12, 0),
+          end: new Date(y, m, d, 14, 0),
+          allDay: false,
+          backgroundColor: '#00c0ef', // Info (aqua)
+          borderColor: '#00c0ef' // Info (aqua)
+        },
+        {
+          title: 'Birthday Party',
+          start: new Date(y, m, d + 1, 19, 0),
+          end: new Date(y, m, d + 1, 22, 30),
+          allDay: false,
+          backgroundColor: '#00a65a', // Success (green)
+          borderColor: '#00a65a' // Success (green)
+        },
+        {
+          title: 'Click for Google',
+          start: new Date(y, m, 28),
+          end: new Date(y, m, 29),
+          url: 'http://google.com/',
+          backgroundColor: '#3c8dbc', // Primary (light-blue)
+          borderColor: '#3c8dbc' // Primary (light-blue)
+        }
+      ]
+
+      this.events = events
+    }
+  }
 }
 </script>
