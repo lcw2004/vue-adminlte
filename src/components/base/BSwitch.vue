a33564489b4b0f01eb5327a72e873901b71e06e9
李昌文 <lcw2004@163.com>
2017-03-15 13:18:05 +0800

    bootswitch

--- src/components/base/BSwitch.vue
+++ src/components/base/BSwitch.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <div class="switch switch-large">
-      <input id="inputEmail" type="checkbox" checked />
+      <input id="inputEmail" type="checkbox" checked :data-size="size"/>
     </div>
   </div>
 </template>
 
 <script>
+require('bootstrap-switch')
+let $ = require('jquery')
+
 export default {
   name: 'BSwitch',
+  props: {
+    label: {
+      type: String,
+      required: false
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    }
+  },
   data: function () {
     return {
     }
   },
   mounted: function () {
+    $('#inputEmail').bootstrapSwitch()
   }
 }
 </script>
