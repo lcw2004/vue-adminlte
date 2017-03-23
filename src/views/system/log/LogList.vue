3ecb2fb0fcddcefdabfdb833d99f20b67ba77eeb
李昌文 <lcw2004@163.com>
2017-03-23 20:45:18 +0800

    日志列表

--- src/views/system/log/LogList.vue
+++ src/views/system/log/LogList.vue
@@ -5,6 +5,39 @@
       <h3 class="box-title">系统日志</h3>
     </div>
     <div class="box-body">
+      <div class="row row-margin-bottom">
+        <div class="col-md-12">
+          <form class="form-inline" @keyup.enter="query()">
+						<div class="form-group">
+							<label class="control-label">URI</label>
+							<input class="form-control inline-block" type="text" v-model="param.requestUri">
+						</div>
+						<div class="form-group">
+							<label class="control-label">操作用户</label>
+							<input class="form-control inline-block" type="text" v-model="param.createByName" placeholder="姓名/登录名">
+						</div>
+						<div class="form-group">
+							<label class="control-label">时间</label>
+							<div class='input-group date'>
+								<input type='text' class="form-control form-date" v-model="param.beginDate"/>
+								<span class="input-group-addon">
+									<span class="glyphicon glyphicon-calendar"></span>
+								</span>
+							</div>
+							-
+							<div class='input-group date'>
+								<input type='text' class="form-control form-date" v-model="param.endDate"/>
+								<span class="input-group-addon">
+									<span class="glyphicon glyphicon-calendar"></span>
+								</span>
+							</div>
+						</div>
+						<div class="form-group">
+							<a class="btn btn-primary" @click="query()">查询</a>
+						</div>
+					</form>
+        </div>
+      </div>
       <div class="row">
         <div class="col-md-12">
           <table class="table table-bordered table-hover">
@@ -31,7 +64,6 @@
               </tr>
             </tbody>
           </table>
-
           <pagination :page="page" @page-no="param.pageNo = arguments[0]" @page-size="param.pageSize = arguments[0]"></pagination>
         </div>
       </div>
