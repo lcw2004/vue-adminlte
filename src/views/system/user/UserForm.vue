4b1d0d2fdc3f85cd19ad78bba7d25f8f20702ccf
李昌文 <lcw2004@163.com>
2017-04-07 23:41:21 +0800

    什么都没做

--- src/views/system/user/UserForm.vue
+++ src/views/system/user/UserForm.vue
@@ -6,112 +6,82 @@
     </div>
     <div class="box-body">
       <form class="form-horizontal">
-        <div class="form-group">
-						<label class="col-sm-2 control-label">归属公司</label>
-						<div class="col-sm-4">
-							<div class="input-group">
-								<input type="text" class="form-control" v-model="obj.company.name"/>
-								<span class="input-group-btn">
+        <div class="form-group" v-render.r="归属公司">
+          <label class="col-sm-2 control-label">归属公司</label>
+          <div class="col-sm-4">
+            <div class="input-group">
+              <input type="text" class="form-control" v-model="obj.company.name" v-validate="'required'" name="归属公司" />
+              <span class="input-group-btn">
 									<button class="btn btn-info" type="button" @click="companyTreeModalConfig.show = true">选择</button>
 								</span>
-							</div>
-              <SelectOfficeModal :config="companyTreeModalConfig" v-model="obj.company"></SelectOfficeModal>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">归属部门</label>
-						<div class="col-sm-4">
-							<div class="input-group">
-								<input type="text" class="form-control" v-model="obj.office.name"/>
-								<span class="input-group-btn">
+            </div>
+            <SelectOfficeModal :config="companyTreeModalConfig" v-model="obj.company"></SelectOfficeModal>
+          </div>
+        </div>
+        <div class="form-group" v-render.r="归属部门">
+          <label class="col-sm-2 control-label">归属部门</label>
+          <div class="col-sm-4">
+            <div class="input-group">
+              <input type="text" class="form-control" v-model="obj.office.name" v-validate="'required'" name="归属部门" />
+              <span class="input-group-btn">
 									<button class="btn btn-info" type="button" @click="officeTreeModalConfig.show = true">选择</button>
 								</span>
-							</div>
-              <SelectOfficeModal :config="officeTreeModalConfig" v-model="obj.office"></SelectOfficeModal>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">工号</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.no"/>
-						</div>
-					</div>
-          <div class="form-group">
-            <label class="control-label col-md-4">测试</label>
-            <div class="col-md-8">
-              <div class="controls">
-                {{ checkboxValue }}
-                <div class="checkbox inline">
-                  <label>
-                    <Checkbox data="1" v-model="checkboxValue"/>1
-                  </label>
-                </div>
-                <div class="checkbox inline">
-                  <label>
-                    <input type="checkbox" value="2" v-model="checkboxValue" v-checkbox>2
-                  </label>
-                </div>
-                <div class="checkbox inline">
-                  <label>
-                    <input type="checkbox" value="3" v-model="checkboxValue">3
-
-                  </label>
-                </div>
-                <div class="checkbox inline">
-                  <label>
-                    <input type="checkbox" value="4" v-model="checkboxValue">4
-
-                  </label>
-                </div>
-              </div>
             </div>
+            <SelectOfficeModal :config="officeTreeModalConfig" v-model="obj.office"></SelectOfficeModal>
           </div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">姓名</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.name"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">登录名</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.loginName"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">邮箱</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.email"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">电话</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.phone"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">手机</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" v-model="obj.mobile"/>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">角色</label>
-						<div class="col-sm-8">
-							<div class="checkbox">
-								<label v-for="role of roles.list">
+        </div>
+        <div class="form-group" v-render.r.i="工号">
+          <label class="col-sm-2 control-label">工号</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.no"  v-validate="'required'" name="工号"/>
+          </div>
+        </div>
+        <div class="form-group" v-render.r="姓名">
+          <label class="col-sm-2 control-label">姓名</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.name" v-validate="'required'" name="姓名"/>
+          </div>
+        </div>
+        <div class="form-group" v-render.r="登录名">
+          <label class="col-sm-2 control-label">登录名</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.loginName" v-validate="'required'" name="登录名"/>
+          </div>
+        </div>
+        <div class="form-group" v-render="邮箱">
+          <label class="col-sm-2 control-label">邮箱</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.email" v-validate="'email'" name="邮箱"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">电话</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.phone" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">手机</label>
+          <div class="col-sm-4">
+            <input type="text" class="form-control" v-model="obj.mobile" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">角色</label>
+          <div class="col-sm-8">
+            <div class="checkbox">
+              <label v-for="role of roles.list">
 									<input type="checkbox" :value="role.id" v-model="obj.roleIdList"> {{ role.name }}
 								</label>
-							</div>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="col-sm-2 control-label">备注</label>
-						<div class="col-sm-4">
-							<textarea type="text" class="form-control" v-model="obj.remarks"></textarea>
-						</div>
-					</div>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-2 control-label">备注</label>
+          <div class="col-sm-4">
+            <textarea type="text" class="form-control" v-model="obj.remarks"></textarea>
+          </div>
+        </div>
       </form>
     </div>
     <div class="box-footer">
